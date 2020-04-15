b27e0ca2dd0ddc4170c415ed466bd977e6b0cda5
YaroslavWeb <retro.bot220@gmail.ru>
2020-04-15 11:03:50 +0800

    add links

--- script.js
+++ script.js
@@ -2,45 +2,45 @@
 let options = {
     value: 'CALL ME <i class="fab fa-skype"></i>',
     color: '00A3E0',
-    arrColor:[0,163,224],
-    blur:0,
-    delay:2,
-    fontFamily:'Bahnschrift',
-    fontSize:81,
-    animation: (arrColor,blur)=>{
-        return(
-`@keyframes neonAnim {
+    arrColor: [0, 163, 224],
+    blur: 0,
+    delay: 2,
+    fontFamily: 'Bahnschrift',
+    fontSize: 81,
+    animation: (arrColor, blur) => {
+        return (
+            `@keyframes neonAnim {
     from {
         text-shadow: 
-          0 0 ${blur+10}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.5),
-          0 0 ${blur+40}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.5),
-          0 0 ${blur+100}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.5);
+          0 0 ${blur+10}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.7),
+          0 0 ${blur+40}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.7),
+          0 0 ${blur+100}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.7);
       }
       to {
         text-shadow: 
-          0 0 ${blur+5}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.5),
-          0 0 ${blur+20}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.5),
-          0 0 ${blur+60}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.5);
+          0 0 ${blur+5}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.7),
+          0 0 ${blur+20}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.7),
+          0 0 ${blur+60}px rgba(${arrColor[0]},${arrColor[1]},${arrColor[2]},.7);
       }
 }`
         )
-    } 
+    }
 }
 // Codemirror init
-let CSScode = CodeMirror(document.getElementById('code-css-preview'), {
-    theme: "darcula",
-    mode: "css",
-    lineNumbers: true,
-    readOnly: true,
-    value: 
-`.neon {
+let CSScode = CodeMirror(
+    document.getElementById('code-css-preview'), {
+        theme: "darcula",
+        mode: "css",
+        lineNumbers: true,
+        readOnly: true,
+        value: `.neon {
     animation: neonAnim 2s alternate-reverse infinite;
     color: #${options.color};
     font-family: ${options.fontFamily};
     font-size: ${options.fontSize}px;
 }
 ${options.animation(options.arrColor, options.blur)}`
-})
+    })
 CSScode.setSize("100%", 400)
 
 let HTMLcode = CodeMirror(document.getElementById('code-html-preview'), {
@@ -49,8 +49,7 @@ let HTMLcode = CodeMirror(document.getElementById('code-html-preview'), {
     htmlMode: true,
     lineNumbers: true,
     readOnly: true,
-    value: 
-`<div class="neon">
+    value: `<div class="neon">
     ${options.value}
 </div>`,
 })
@@ -59,20 +58,20 @@ HTMLcode.setSize("100%", 100)
 // Init preview values
 $('#text-preview').html(options.value)
 $('#text-preview').css({
-    'animation':`neonAnim ${options.delay}s alternate-reverse infinite`,
+    'animation': `neonAnim ${options.delay}s alternate-reverse infinite`,
     'color': `#${options.color}`,
     'font-family': options.fontFamily,
-    'font-size': options.fontSize+'px'
+    'font-size': options.fontSize + 'px'
 })
 $('#animationKeyframes').text(options.animation(options.arrColor, options.blur))
-    
+
 // EVENTS
 // Change text preview && HTML code
 $('#input-value-preview').keyup(event => {
     options.value = event.target.value
     $('#text-preview').html(options.value)
     HTMLcode.setValue(
-`<div class="neon">
+        `<div class="neon">
     ${options.value}
 </div>`)
 })
@@ -90,7 +89,7 @@ setPreviewColor = (picker) => {
     })
     $('#animationKeyframes').text(options.animation(options.arrColor, options.blur))
     CSScode.setValue(
-`.neon {
+        `.neon {
     animation: neonAnim 2s alternate-reverse infinite;
     color: #${options.color};
     font-family: ${options.fontFamily};
@@ -101,13 +100,13 @@ ${options.animation(options.arrColor, options.blur)}`
 }
 
 // Change font-family && CSS code
-$('#select-family-preview').change((event)=>{
+$('#select-family-preview').change((event) => {
     options.fontFamily = event.target.value
     $('#text-preview').css({
         'font-family': options.fontFamily,
     })
     CSScode.setValue(
-`.neon {
+        `.neon {
     animation: neonAnim 2s alternate-reverse infinite;
     color: #${options.color};
     font-family: ${options.fontFamily};
@@ -118,14 +117,14 @@ ${options.animation(options.arrColor, options.blur)}`
 })
 
 // Change font-size && CSS code
-$('#input-size-preview').change((event)=>{
-    
+$('#input-size-preview').change((event) => {
+
     options.fontSize = event.target.value
     $('#text-preview').css({
-        'font-size':options.fontSize+'px'
+        'font-size': options.fontSize + 'px'
     })
     CSScode.setValue(
-`.neon {
+        `.neon {
     animation: neonAnim 2s alternate-reverse infinite;
     color: #${options.color};
     font-family: ${options.fontFamily};
@@ -136,11 +135,11 @@ ${options.animation(options.arrColor, options.blur)}`
 })
 
 // Change blur && CSS code
-$('#input-blur-preview').change((event)=>{
+$('#input-blur-preview').change((event) => {
     options.blur = Number(event.target.value)
     $('#animationKeyframes').text(options.animation(options.arrColor, options.blur))
     CSScode.setValue(
-`.neon {
+        `.neon {
     animation: neonAnim 2s alternate-reverse infinite;
     color: #${options.color};
     font-family: ${options.fontFamily};
@@ -151,13 +150,13 @@ ${options.animation(options.arrColor, options.blur)}`
 })
 
 // Change delay && CSS code
-$('#input-delay-preview').change((event)=>{
+$('#input-delay-preview').change((event) => {
     options.delay = Number(event.target.value)
     $('#text-preview').css({
-        'animation':`neonAnim ${options.delay}s alternate-reverse infinite`
+        'animation': `neonAnim ${options.delay}s alternate-reverse infinite`
     })
     CSScode.setValue(
-`.neon {
+        `.neon {
     animation: neonAnim ${options.delay}s alternate-reverse infinite;
     color: #${options.color};
     font-family: ${options.fontFamily};
@@ -168,29 +167,29 @@ ${options.animation(options.arrColor, options.blur)}`
 })
 
 // Add icon && HTML code
-let addIcon = (icon) =>{
+let addIcon = (icon) => {
     options.value += icon
     $('#input-value-preview').val(options.value)
     $('#text-preview').html(options.value)
     HTMLcode.setValue(
-`<div class="neon">
+        `<div class="neon">
     ${options.value}
 </div>`)
 }
 
 // Clear input value preview
-$('#clear-value-preview').click(()=>{
-options.value = ''
-$('#input-value-preview').val(options.value)
-$('#text-preview').html(options.value)
-HTMLcode.setValue(
-`<div class="neon">
+$('#clear-value-preview').click(() => {
+    options.value = ''
+    $('#input-value-preview').val(options.value)
+    $('#text-preview').html(options.value)
+    HTMLcode.setValue(
+        `<div class="neon">
     ${options.value}
 </div>`)
 })
 
 // Copy code by button
-let copyToClipboard = (text)=>{
+let copyToClipboard = (text) => {
     let textarea = document.createElement("textarea");
     document.body.appendChild(textarea);
     textarea.value = text;
@@ -199,11 +198,9 @@ let copyToClipboard = (text)=>{
     document.body.removeChild(textarea);
 }
 
-$('.copy-css').click(()=>{
+$('.copy-css').click(() => {
     copyToClipboard(CSScode.getValue())
 })
-$('.copy-html').click(()=>{
+$('.copy-html').click(() => {
     copyToClipboard(HTMLcode.getValue())
 })
-
-
